3388406e4d1cb0e9c159c1462148d2dacf01039a
Ushan Edirisinghe <malindaushan999@gmail.com>
2023-11-18 13:41:43 +0530

    Initial commit

---
+++ Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace $safeprojectname$;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationSerivices(this IServiceCollection services)
+    {
+        var mapsterConfig = TypeAdapterConfig.GlobalSettings;
+
+        services.AddSingleton(mapsterConfig);
+        services.AddScoped<IMapper, ServiceMapper>();
+        services.AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
